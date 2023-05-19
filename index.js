c17f42aa54f73b31d62df2c9a1ea12bd927cb9a7
AnderDev513 <andersondiasleite@icloud.com>
2023-05-19 19:25:19 -0300

    finished my first project node

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const uuid = require('uuid')
+
+const port = 3000
+const app = express()
+app.use(express.json())
+
+/* 
+        comando executor do servidor "npm run (nome do script)"        
+
+        - Query params => MediaQueryListEvent.com/users?nome=anderson&age=27  // FILTROS
+        - Route params => /users/2   // BUSCAR, DELETAR OU ATUALIZAR ALOGO ESPECÍFICO
+        - Request Body => { "name:"Anderson", "age":}
+
+        - GET          => Buscar informações no bakc-end
+        - POST         => Criar informações no back-end
+        - PUT / PATCH  => Alterar/Atualizar informações no bakc-end
+        - DELETE       => Deletar informações no back-end
+
+        - Middleware => INTERCEPTADOR => Tem o poder de parar ou alterar dados de requisição
+ */
+
+
+const users = []
+
+const checkUserId = (request, response, next) => { 
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if(index < 0){
+        return response.status(404).json({ message: "user not found"})
+    }
+
+    request.userIndex = index
+    request.userId = id
+
+    next()
+}
+
+app.get('/users', (request, response) => {
+
+    return response.json(users)
+})
+
+app.post('/users', (request, response) => {
+    const { name, age } = request.body
+
+    const user = { id: uuid.v4(), name, age }
+
+    users.push(user)
+
+    return response.status(201).json(user)
+})
+
+app.put('/users/:id', checkUserId, (request, response) => {
+    const { name, age } = request.body
+    const index = request.userIndex
+    const id = request.userId
+
+    const updatedUser = { id, name, age }
+ 
+    users[index] = updatedUser
+    
+    return response.json(updatedUser)
+})
+
+app.delete('/users/:id',checkUserId, (request, response) => {
+    const index = request.userIndex
+
+    users.splice(index,1)
+
+    return response.status(204).json(users)
+})
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`🌎 Server started on port ${port} 🚀`)
+})
